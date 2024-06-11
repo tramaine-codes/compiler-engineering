1695e4ef7bc6e7b4b4cfb50ef93c15f44bf02e8d
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-10 23:16:17 -0400

    feat: add if expression

---
+++ eva/test/eva/if.test.ts
@@ -0,0 +1,20 @@
+import { beforeEach, expect, test } from 'bun:test';
+import { Eva } from '../../src/Eva.js';
+
+let eva: Eva;
+
+beforeEach(() => {
+  eva = new Eva();
+});
+
+test('add version to global environment', () => {
+  expect(
+    eva.eval([
+      'begin',
+      ['var', 'x', 10],
+      ['var', 'y', 0],
+      ['if', ['>', 'x', 10], ['set', 'y', 20], ['set', 'y', 30]],
+      'y',
+    ])
+  ).toEqual(30);
+});
